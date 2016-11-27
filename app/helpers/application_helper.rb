e758f4c2973a1943b2a8798cb0d28e3f8ad64d7a
Eirik Wiig <Eirikwiig@gmail.com>
2016-11-27 17:29:25 +0000

    Update bootstrap alert css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   BOOTSTRAP_FLASH_MSG = {
     success: 'alert-success',
     error: 'alert-error',
-    alert: 'alert-block',
+    alert: 'alert-warning',
     notice: 'alert-info'
   }.freeze
 
